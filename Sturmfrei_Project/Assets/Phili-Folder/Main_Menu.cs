3b47a0be52610029f8affd924568c7e3efb7dfe9
Azanen <60452086+Azanen@users.noreply.github.com>
2022-04-19 19:45:42 -0400

    Fixe Hub Menu

--- Sturmfrei_Project/Assets/Phili-Folder/Main_Menu.cs
+++ Sturmfrei_Project/Assets/Phili-Folder/Main_Menu.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class Main_Menu : MonoBehaviour
 {
+    public UnityEvent eventMain;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.None; //fait reapparaitre le curseur
-
+        eventMain.Invoke();
     }
 
 public void RemoveCursor()
