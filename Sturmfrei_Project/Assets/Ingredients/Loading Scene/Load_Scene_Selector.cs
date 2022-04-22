007a9bdcc33bdff5e27ff04bca4ac3739878e1c2
Azanen <60452086+Azanen@users.noreply.github.com>
2022-04-22 15:22:06 -0400

    Update Load_Scene_Selector.cs

--- Sturmfrei_Project/Assets/Ingredients/Loading Scene/Load_Scene_Selector.cs
+++ Sturmfrei_Project/Assets/Ingredients/Loading Scene/Load_Scene_Selector.cs
@@ -6,12 +6,25 @@ public class Load_Scene_Selector : MonoBehaviour
 {
     // list des scene en string
     public UnityEvent Test;
+    public PlayerMovement player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            Test.Invoke();
+            player.StartFadeWhite();
+            StartCoroutine(LoadWithFadeToWhite());
         }
+    }
+
+    IEnumerator LoadWithFadeToWhite()
+    {
+        yield return new WaitForSeconds(2);
+        Test.Invoke();
     }
 }
