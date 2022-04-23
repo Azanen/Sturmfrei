64a4d6ad6200419fc111f2cb6699bb2f7f6b8e5f
GrimmWyvern <98108077+GrimmWyvern@users.noreply.github.com>
2022-04-22 20:00:33 -0400

    Menu pause complet avec options et controles

--- Sturmfrei_Project/Assets/UI/MenuPause.cs
+++ Sturmfrei_Project/Assets/UI/MenuPause.cs
@@ -8,18 +8,31 @@ public class MenuPause : MonoBehaviour
 {
     public static bool isPaused = false;
     public GameObject menuPauseUI;
+    public GameObject menuOptionsUI;
+    public GameObject menuControlsUI;
     public GameObject player;
     public string menuPrincipal = "MenuPrincipal";
 
     public GameObject pauseFirstButton;
+    public GameObject optionsFirstButton;
+    public GameObject controlsFirstButton;
+
+    /*public GameObject contourInvertFly;
+    private bool toggleBoutFly;
+    public GameObject contourInvertCam;
+    private bool toggleBoutCam;
+    public GameObject SaveSys;
+    */
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+       // ToggleCam();
+       // ToggleFly();
     }
     void Update()
     {
-       if (Input.GetButtonDown("Pause"))
+        if (Input.GetButtonDown("Pause"))
         {
             if (isPaused == true)
             {
@@ -35,9 +48,12 @@ public class MenuPause : MonoBehaviour
     public void Reprendre() //fonction pour le bouton "reprendre" aussi
     {
         menuPauseUI.SetActive(false);
-        Time.timeScale = 1f; 
+        menuControlsUI.SetActive(false);
+        menuOptionsUI.SetActive(false);
+
+        Time.timeScale = 1f;
         isPaused = false;
-        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.lockState = CursorLockMode.Locked;
         player.GetComponent<PlayerMovement>().enabled = true;
     }
 
@@ -56,7 +72,7 @@ public class MenuPause : MonoBehaviour
     {
         //Debug.Log("loadingMenuPrincipal");
         SceneManager.LoadScene(menuPrincipal);
-        Time.timeScale = 1f; 
+        Time.timeScale = 1f;
     }
 
     public void Quitter()
@@ -64,4 +80,52 @@ public class MenuPause : MonoBehaviour
         //Debug.Log("Quitte le jeu");
         Application.Quit();
     }
+
+    public void OpenOption()
+    {
+        menuOptionsUI.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);    //clear les selection
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton); //cree nouvelle selection (pour manette)
+    }
+
+    public void CloseOption()
+    {
+        menuOptionsUI.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);    //clear les selection
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton); //cree nouvelle selection (pour manette)
+    }
+
+    public void OpenControls()
+    {
+        menuControlsUI.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);    //clear les selection
+        EventSystem.current.SetSelectedGameObject(controlsFirstButton); //cree nouvelle selection (pour manette)
+    }
+
+    public void CloseControls()
+    {
+        menuControlsUI.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);    //clear les selection
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton); //cree nouvelle selection (pour manette)
+    }
+    /*
+    public void ToggleCam()
+    {
+        if (SaveSys.GetComponent<Save_System_Proto>().ReturnCamInverted() > 0) 
+        {
+            toggleBoutCam = true;
+            contourInvertCam.SetActive(toggleBoutCam);
+        }
+        else { toggleBoutCam = false; contourInvertCam.SetActive(toggleBoutCam);}
+    }
+
+    public void ToggleFly()
+    {
+        if (SaveSys.GetComponent<Save_System_Proto>().ReturnFlyingInverted() > 0) 
+        { 
+            toggleBoutFly = true;
+            contourInvertCam.SetActive(toggleBoutFly);
+        }
+        else { toggleBoutFly = false; contourInvertFly.SetActive(toggleBoutFly);}
+    }*/
 }
